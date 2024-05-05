a61b1cd6fa180801930a524f9fbbe06f08ddcb3c
Victor Bankole <bankolevictor77@gmail.com>
2024-05-05 19:55:59 +0100

    add client table

---
+++ Layout/Dashboard/pages/clientMgtDashboard.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+  $("#myTable").DataTable({
+    paging: true, // Allow data to be split into pages
+    lengthMenu: [5, 10, 25, 50, 75, 100], // Number of records per page options
+    searching: true, // Enable search box
+    ordering: true, // Enable sorting
+    info: true, // Enable showing information about the table
+    autoWidth: false, // Disable auto width calculation
+    responsive: true, // Enable responsive design
+    dom: "lBfrtip", // Add export buttons to the table
+    buttons: ["copy", "csv", "excel", "pdf", "print"],
+    language: {
+      search: "Search:",
+      lengthMenu: "Display _MENU_ records per page",
+      info: "Showing _START_ to _END_ of _TOTAL_ entries",
+      infoEmpty: "Showing 0 to 0 of 0 entries",
+      infoFiltered: "(filtered from _MAX_ total entries)",
+      paginate: {
+        first: "First",
+        last: "Last",
+        next: "Next",
+        previous: "Previous",
+      },
+    },
+
+    initComplete: function () {
+      // Add filtering options for client type
+      this.api()
+        .columns(1)
+        .every(function () {
+          var column = this;
+          var select = $(
+            '<select class="form-select"><option value="">All Client Types</option></select>'
+          )
+            .appendTo($("#client-type-filter"))
+            .on("change", function () {
+              var val = $.fn.dataTable.util.escapeRegex($(this).val());
+
+              column.search(val ? "^" + val + "$" : "", true, false).draw();
+            });
+
+          column
+            .data()
+            .unique()
+            .sort()
+            .each(function (d, j) {
+              select.append('<option value="' + d + '">' + d + "</option>");
+            });
+        });
+
+      // Add filtering options for status
+      this.api()
+        .columns(4)
+        .every(function () {
+          var column = this;
+          var select = $(
+            '<select class="form-select"><option value="">All Statuses</option></select>'
+          )
+            .appendTo($("#status-filter"))
+            .on("change", function () {
+              var val = $.fn.dataTable.util.escapeRegex($(this).val());
+
+              column.search(val ? "^" + val + "$" : "", true, false).draw();
+            });
+
+          select
+            .append('<option value="enable">Enable</option>')
+            .append('<option value="disable">Disable</option>');
+        });
+    },
+  });
+  // Trigger export actions
+  $("#export-copy").click(function () {
+    $("#myTable").DataTable().button(".buttons-copy").trigger();
+  });
+  $("#export-csv").click(function () {
+    $("#myTable").DataTable().button(".buttons-csv").trigger();
+  });
+  $("#export-excel").click(function () {
+    $("#myTable").DataTable().button(".buttons-excel").trigger();
+  });
+  $("#export-pdf").click(function () {
+    $("#myTable").DataTable().button(".buttons-pdf").trigger();
+  });
+  $("#export-print").click(function () {
+    $("#myTable").DataTable().button(".buttons-print").trigger();
+  });
+});
